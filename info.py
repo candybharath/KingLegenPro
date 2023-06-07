e7d65a31033c675e55e08b004f9e0d7c9a4e9a14
PYRO_PRIEST!! <85893956+PYROPRIEST@users.noreply.github.com>
2023-06-07 21:22:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6141743333:AAHrC11ZFQa-XJIy0K6zrn5j1sItD1P
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/ad51af5d7b62b176dd4e8.jpg https://telegra.ph/file/337baddb6be47b02ce895.jpg https://telegra.ph/file/78e1de114681234d24c84.jpg https://telegra.ph/file/16aeb75f2aa9f1b4b38ee.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/10009136df7620d9d3e5a.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://graph.org/file/5813021ba24f773df40a8.jpg")
 SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/b58f576fed14cd645d2cf.jpg")
 
